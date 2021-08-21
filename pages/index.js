cd1917a76a032c34f893fc0256226ee335977eee
stephen <sflores813@gmail.com>
2021-08-20 23:02:56 -0500

    adding in the top nav bar and starting banner

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Stephens airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    <Header />
+    {/* Banner */}
+    
+
+      
+    </div>
+  )
+}
